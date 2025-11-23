89baf7b3499e7592109fd82dc72cd1e3d9369c18
Collins Kibet <kollcibe05@gmail.com>
2025-11-23 18:41:47 +0300

    created debug.py

---
+++ debug.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import ipdb
+
+from class_file.customer import Customer
+from class_file.coffee import Coffee
+from class_file.order import Order
+
+if __name__ == "__main__":
+    print("Debugger initializing --------------")
+
+    ipdb.set_trace()
